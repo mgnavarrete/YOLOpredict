a667bde9dd2824a15d7b20f88e97c43e961c56b5
mgnavarrete <mgnavarrete@uc.cl>
2024-01-11 11:56:13 -0300

    kml tables

--- corrector.py
+++ corrector.py
@@ -1,8 +1,6 @@
 from correctScripts.correctH import correctH
 from correctScripts.correctYaw import correctYaw
-from correctScripts.correctE import correctE
 from correctScripts.saveGeoMatriz import saveGeoM
-
 from ultralytics import YOLO
 import os
 import cv2
@@ -50,6 +48,33 @@ if __name__ == '__main__':
     select_directories()
     print("Directorio raíz seleccionado")
 
+    # Preprocesar coordenadas en el DataFrame
+    print("Cargando datos de KML...")
+
+    df = pd.read_csv(csv_file_path)
+    print("Datos cargados")
+    
+    for col in ['polyP1', 'polyP2', 'polyP3', 'polyP4']:
+        df[col] = df[col].apply(lambda x: tuple(map(float, x.split(','))))
+
+    yawKML = df['yaw'].mean()
+    
+    print("Cargando modelo YOLO..")
+    model = YOLO(model_path)
+    print("Modelo cargado")
+
+    zone_number = 19
+    zone_letter = 'S'
+
+    # Define la proyección UTM (incluyendo la zona y el hemisferio)
+    utm_crs = CRS(f"+proj=utm +zone={zone_number} +{'+south' if zone_letter > 'N' else ''} +ellps=WGS84")
+
+    # Define la proyección de latitud/longitud
+    latlon_crs = CRS("EPSG:4326")
+
+    # Crear un objeto Transformer para la transformación de coordenadas
+    transformer = Transformer.from_crs(utm_crs, latlon_crs, always_xy=True)
+
     for path_root in list_folders:
         path_root = path_root + "PP"
         print(f"Procesando Carpeta:{path_root}")
@@ -63,30 +88,11 @@ if __name__ == '__main__':
         img_names = os.listdir(imgsFolder)
         img_names.sort()
 
-
-        zone_number = 19
-        zone_letter = 'S'
-
-        # Define la proyección UTM (incluyendo la zona y el hemisferio)
-        utm_crs = CRS(f"+proj=utm +zone={zone_number} +{'+south' if zone_letter > 'N' else ''} +ellps=WGS84")
-
-        # Define la proyección de latitud/longitud
-        latlon_crs = CRS("EPSG:4326")
-
-        # Crear un objeto Transformer para la transformación de coordenadas
-        transformer = Transformer.from_crs(utm_crs, latlon_crs, always_xy=True)
-
         if not os.path.exists(metadatanew_path):
                 os.mkdir(metadatanew_path)
-        # Preprocesar coordenadas en el DataFrame
-        print("Cargando datos de KML...")
+        
 
-        df = pd.read_csv(csv_file_path)
-        print("Datos cargados")
-
-        print("Cargando modelo YOLO..")
-        model = YOLO(model_path)
-        print("Modelo cargado")
+       
 
         print("Iniciando análisis de imágenes...")
 
