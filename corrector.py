71de1dfea13322c317f48b0faf40bbba4c5e566b
mgnavarrete <mgnavarrete@uc.cl>
2024-01-15 17:38:51 -0300

    xd

--- corrector.py
+++ corrector.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
         if planta == '3':
             print("Ajustando Planta Campos del Sol...")
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
-            correctHCDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho, areaUmb)
+            correctHCDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho)
             correctYawCDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctECDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
