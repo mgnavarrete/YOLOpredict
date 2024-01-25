bdbe974be392d6d475111aced759a35b3f3dac41
mgnavarrete <mgnavarrete@uc.cl>
2024-01-25 15:13:31 -0300

    ajueste sdl

--- corrector.py
+++ corrector.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 
 
 def deleteGeoNp(geonp_path):
-        for filename in os.listdir(geonp_path):
+        for filename in tqdm(os.listdir(geonp_path), desc="Deleting GeoNumpy:"):
             file_path = os.path.join(geonp_path, filename)
             try:
                 # Si es un archivo, lo elimina
@@ -88,6 +88,7 @@ if __name__ == '__main__':
     print("         2. Finis Terrae Extensión (FIX)")
     print("         3. Campos del Sol (CDS)")
     print("         4. Lalakama (LLK)")
+    print("         5. Sol de Lila (SDL)")
     print("         x. Salir")
     planta = input("Seleccione una opción: ")
     if planta == '1':
@@ -107,7 +108,12 @@ if __name__ == '__main__':
         difUmb = 100000000
     
         csv_file_path = 'kmlTables/LLKCorrection.csv'
+        
+    elif planta == '5':
+        areaUmb = 0
+        difUmb = 100000000
     
+        csv_file_path = 'kmlTables/SDL - Trackers numerados.csv'
         
     else:
         exit()
@@ -185,6 +191,7 @@ if __name__ == '__main__':
             correctYaw(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctE(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            saveKML(img_names, path_root)
             deleteGeoNp(geonp_path)
         if planta == '2':
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
@@ -193,6 +200,7 @@ if __name__ == '__main__':
             correctYaw(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctE(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            saveKML(img_names, path_root)
             deleteGeoNp(geonp_path)
         
         if planta == '3':
@@ -203,6 +211,7 @@ if __name__ == '__main__':
             correctYawCDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctECDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            saveKML(img_names, path_root)
             deleteGeoNp(geonp_path)
         elif planta == '4':
             print("Ajustando Planta Lalakama...")
@@ -215,7 +224,19 @@ if __name__ == '__main__':
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             # correctELLK2(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
             saveKML(img_names, path_root)
-            # deleteGeoNp(geonp_path)
+            deleteGeoNp(geonp_path)
         
-
+        elif planta == '5':
+            print("Ajustando Planta Sol de Lila...")
+            resetMD(img_names, metadata_path)
+            saveGeoM(img_names, metadata_path, geonp_path, path_root)   
+            correctHLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho, areaUmb, path_root)
+            correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
+            # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
+            # correctECDS(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            # adjustMD(img_names, metadata_path, 'offset_E', 0)
+            # correctELLK2(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            saveKML(img_names, path_root)
+            deleteGeoNp(geonp_path)
+            
     print("Todas la carpetas OK")
