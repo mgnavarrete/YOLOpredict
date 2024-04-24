713024417c19d73b3b3fe25d17b59e84e54d3af8
mgnavarrete <mgnavarrete@uc.cl>
2024-04-24 18:17:37 -0400

    just h for MNZ

--- corrector.py
+++ corrector.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
             
             resetMD(img_names, metadata_path, 'all')
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
-            correctHLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, 0.0025, areaUmb, path_root)
+            correctHLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho, areaUmb, path_root)
             # correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             # adjustMD(img_names, metadata_path, 'offset_E', -3)
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
