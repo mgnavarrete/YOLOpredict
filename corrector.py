7553244bc83fa512a71eb4fceeb98d7df9485e18
mgnavarrete <mgnavarrete@uc.cl>
2024-04-24 16:45:41 -0400

    just h for MNZ

--- corrector.py
+++ corrector.py
@@ -256,10 +256,10 @@ if __name__ == '__main__':
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctHLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, 0.0025, areaUmb, path_root)
             # correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
-            # adjustMD(img_names, metadata_path, 'offset_E', -5)
+            adjustMD(img_names, metadata_path, 'offset_E', -3)
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
-            correctELLK2(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
+            # correctELLK2(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
             saveKML(img_names, path_root)
-            deleteGeoNp(geonp_path)
+            # deleteGeoNp(geonp_path)
             
     print("Todas la carpetas OK")
