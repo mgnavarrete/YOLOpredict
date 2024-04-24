03edec5338eca3c3b6ec9f1e593df5f241750342
mgnavarrete <mgnavarrete@uc.cl>
2024-04-24 17:17:11 -0400

    just h for MNZ

--- corrector.py
+++ corrector.py
@@ -253,10 +253,10 @@ if __name__ == '__main__':
             print("Ajustando Planta El Manzano...")
             
             resetMD(img_names, metadata_path, 'all')
-            saveGeoM(img_names, metadata_path, geonp_path, path_root)   
+            # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             correctHLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, 0.0025, areaUmb, path_root)
             # correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
-            adjustMD(img_names, metadata_path, 'offset_E', -3)
+            # adjustMD(img_names, metadata_path, 'offset_E', -3)
             # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             # correctELLK2(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model)
             saveKML(img_names, path_root)
